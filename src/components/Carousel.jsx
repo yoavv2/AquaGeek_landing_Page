a26327ecf1734c68cda01f243f117448b7cb2521
yoavv2 <23hutc8@gmail.com>
2024-11-05 17:20:49 +0200

    add meta tags

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 const images = [
-  { id: 1, src: '/gallery_image1.jpg', alt: 'Image 1' },
-  { id: 2, src: '/gallery_image2.jpg', alt: 'Image 2' },
-  { id: 3, src: '/gallery_image3.png', alt: 'Image 3' },
-  { id: 4, src: '/gallery_image4.png', alt: 'Image 4' },
-  { id: 5, src: '/gallery_image5.jpg', alt: 'Image 5' },
-  { id: 6, src: '/gallery_image6.jpg', alt: 'Image 6' },
-  { id: 7, src: '/gallery_image7.jpg', alt: 'Image 7' },
+  { id: 1, src: '/gallery_image1.jpg', alt: 'project 1' },
+  { id: 2, src: '/gallery_image2.jpg', alt: 'project 2' },
+  { id: 3, src: '/gallery_image3.png', alt: 'project 3' },
+  { id: 4, src: '/gallery_image4.png', alt: 'project 4' },
+  { id: 5, src: '/gallery_image5.jpg', alt: 'project 5' },
+  { id: 6, src: '/gallery_image6.jpg', alt: 'project 6' },
+  { id: 7, src: '/gallery_image7.jpg', alt: 'project 7' },
 ];
 
 export default function Carousel({ options = { loop: true } }) {
